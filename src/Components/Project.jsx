eb289321137c99d4c1599f0139169f5fd496c29c
niyicodes <elijahtost@gmail.com>
2022-10-28 05:43:08 +0100

    more changes

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Project = ({ item }) => {
-  return (
-    <div key={item.id} className='flex flex-col items-center text-center'>
-      <div className='mb-8'>
-        <img className='rounded-2xl' src={item.image} alt='' />
-      </div>
-      <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
-      <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
-      <p className='text-base max-w-md'>
-        {item.stack}
-      </p>
-    </div>
-  );
+ return (
+  <div key={item.id} className="flex flex-col items-center text-center">
+   <div className="mb-8">
+    <img className="rounded-2xl w-64 h-64 object-cover" src={item.image} alt="" />
+   </div>
+   <h3 className="text-2xl font-semibold capitalize mb-3">{item.name}</h3>
+   <p className="capitalize text-accent text-sm mb-3">{item.category}</p>
+   <p className="text-base max-w-md">{item.stack}</p>
+  </div>
+ );
 };
 
 export default Project;
--- src/data.js
+++ src/data.js
@@ -1,11 +1,15 @@
 
-// // projects images
-// import Project1 from './assets/projects/p1.webp';
-// import Project2 from './assets/projects/p2.webp';
-// import Project3 from './assets/projects/p3.webp';
-// import Project4 from './assets/projects/p4.webp';
-// import Project5 from './assets/projects/p5.webp';
-// import Project6 from './assets/projects/p6.webp';
+// projects images
+import mamaput from './assets/projects/mamaput.png';
+import mealsearch from './assets/projects/mealsearch.png';
+import niyifruity from './assets/projects/niyifruity.png';
+import musica from './assets/projects/musica.png';
+import mytrivia from './assets/projects/mytrivia.png';
+import numguesser from './assets/projects/numguesser.png';
+import clothingdemo from './assets/projects/clothingdemo.png';
+import branchneat from './assets/projects/branchneat.png';
+import realtypro from './assets/projects/realtypro.png';
+import budget from './assets/projects/budget.png';
 
 // skills images
 import SkillImg1 from "./assets/skills/html5.png";
@@ -41,76 +45,76 @@ export const navigation = [
 
 // projects
 export const projectsData = [
- //  {
- //    id: '1',
- //    image: mamaput,
- //    name: 'MamaPut',
- //    category: "under-review",
- //    stack: "REACT + REDUX"
- //  },
- //  {
- //    id: '2',
- //    image: mealsearch,
- //    name: 'Meal Search',
- //    category: "completed",
- //    stack: "HTML + JS + API"
- //  },
- //  {
- //    id: '3',
- //    image: niyifruity,
- //    name: 'Fuity',
- //    category: 'under-review',
- //    stack: "HTML + BOOTSTRAP"
- //  },
- //  {
- //    id: '4',
- //    image: musica,
- //    name: 'Musica',
- //    category: 'current',
- //    stack:"REACT + FRAMER-MOTION + TAILWIND CSS"
- //  },
- //  {
- //    id: '5',
- //    image: mytrivia,
- //    name: 'Trivia app',
- //    category: 'completed',
- //   stack: "REACT + API"
- //  },
- //  {
- //    id: '6',
- //    image: numguesser,
- //    name: 'Number Guesser',
- //    category: 'completed',
- //    stack: "HTML + JAVASCRIPT"
- //  },
- //  {
- //    id: '7',
- //    image: clothingdemo,
- //    name: 'My Clothing',
- //    category: '',
- //    stack: "REACT + REDUX"
- //  },
- //  {
- //    id: '8',
- //    image: branchneat,
- //    name: 'Branch `n` Eat',
- //    category: 'completed',
- //    stack: "HTML + CSS + JAVASCRIPT"
- //  },
- //  {
- //    id: '9',
- //    image: realtypro,
- //    name: 'Realty Pro',
- //    category: 'under-review',
- //    stack: "HTML + CSS"
- //  },
- //  {
- //    id: '10',
- //    image: budget,
- //    name: 'Expensify',
- //    category: 'under-review',
- //    stack: "REACT + REDUX"
- //  },
+  {
+    id: '1',
+    image: mamaput,
+    name: 'MamaPut',
+    category: "under-review",
+    stack: "REACT + REDUX"
+  },
+  {
+    id: '2',
+    image: mealsearch,
+    name: 'Meal Search',
+    category: "completed",
+    stack: "HTML + JS + API"
+  },
+  {
+    id: '3',
+    image: niyifruity,
+    name: 'Fuity',
+    category: 'under-review',
+    stack: "HTML + BOOTSTRAP"
+  },
+  {
+    id: '4',
+    image: musica,
+    name: 'Musica',
+    category: 'current',
+    stack:"REACT + FRAMER-MOTION + TAILWIND CSS"
+  },
+  {
+    id: '5',
+    image: mytrivia,
+    name: 'Trivia app',
+    category: 'completed',
+   stack: "REACT + API"
+  },
+  {
+    id: '6',
+    image: numguesser,
+    name: 'Number Guesser',
+    category: 'completed',
+    stack: "HTML + JAVASCRIPT"
+  },
+  {
+    id: '7',
+    image: clothingdemo,
+    name: 'My Clothing',
+    category: '',
+    stack: "REACT + REDUX"
+  },
+  {
+    id: '8',
+    image: branchneat,
+    name: 'Branch `n` Eat',
+    category: 'completed',
+    stack: "HTML + CSS + JAVASCRIPT"
+  },
+  {
+    id: '9',
+    image: realtypro,
+    name: 'Realty Pro',
+    category: 'under-review',
+    stack: "HTML + CSS"
+  },
+  {
+    id: '10',
+    image: budget,
+    name: 'Expensify',
+    category: 'under-review',
+    stack: "REACT + REDUX"
+  },
 ];
 
 // projects
